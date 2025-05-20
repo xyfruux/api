b74f594404035fc00512210299cfc89651a9e767
xyfruux <victor.stampe2010@gmail.com>
2025-05-20 21:19:04 +0200

    Change Root Output To Data File

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
+const data = require("./data.json");
+
 const express = require("express");
 const port = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      title: "Blue Mouse",
-      price: 10.99,
-      description: "A blue mouse with a sleek design.",
-    },
-  ]);
+  res.json(data);
 });
 
 app.listen(port, () => {
