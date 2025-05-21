2374f8ac73813a03f7245260225715ca72620237
xyfruux <victor.stampe2010@gmail.com>
2025-05-21 18:23:44 +0200

    V0.2.0⬆️
    Create Filter Using minPrice And maxPrice
    
    commit 102468a690d6c604bf4dae1b277d693a1fe1149c
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 18:16:35 2025 +0200
    
        Respond With Filtered Items
    
    commit c0c10c5858074d97bfbaa3c7b27183b148e72f4b
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 18:11:14 2025 +0200
    
        Convert Values Into An Array
    
    commit 5b65c1b4a20dee2fb7f09562d4e288a3ae84da4c
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 18:10:13 2025 +0200
    
        Filter Data Through Min Max
    
    commit 947995ac28ef69e4b8739ad3e6fbec333bf20c3c
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 17:56:41 2025 +0200
    
        Make Min Max Query An Or Value
    
    commit b1bc1172bddb153646994ab301812299c6fc6a79
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 17:54:06 2025 +0200
    
        Create Min And Max Price Values
    
    commit b2ce9302ad9f34eca3b49d5e81708b8961789864
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 17:44:20 2025 +0200
    
        Change Electronics To electronics
    
    commit e9200fb581da5609b32326f899f4a2c9e5bda20f
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 17:42:05 2025 +0200
    
        Create Category For Data
    
    commit dcb111934a9c6e8c2e8ae0e9c7d7c640d1b1a854
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Wed May 21 17:35:18 2025 +0200
    
        Create An Array With Multiple Items
    
    commit b74f594404035fc00512210299cfc89651a9e767
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Tue May 20 21:19:04 2025 +0200
    
        Change Root Output To Data File
    
    commit dbbd13499f2e932bdf0e045e5a0e7f29f58884fb
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Tue May 20 21:18:49 2025 +0200
    
        Create Data File
    
    commit 76a1d0698b3a1cc93395f5bcbd0f96bbbd25563a
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Tue May 20 21:16:11 2025 +0200
    
        Fix Server Start Path
    
    commit 24415c51f87abde392778d1e00ae7dc5b348e1ea
    Author: xyfruux <victor.stampe2010@gmail.com>
    Date:   Tue May 20 21:14:32 2025 +0200
    
        Move Server Into Src Folder

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const data = require("./data.json");
+
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  const { minPrice, maxPrice } = req.query;
+
+  const filteredItems = data.filter((item) => {
+    return (
+      (!minPrice || item.price >= minPrice) &&
+      (!maxPrice || item.price <= maxPrice)
+    );
+  });
+
+  res.json(filteredItems);
+});
+
+app.listen(port, () => {
+  console.log(`Running on http://localhost:${port}`);
+});
