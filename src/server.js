12b64adbe800cab142f2ed603108776918cde7d2
xyfruux <victor.stampe2010@gmail.com>
2025-05-23 22:47:03 +0200

    Remove /create & /delete On Server

--- src/server.js
+++ src/server.js
@@ -36,32 +36,6 @@ require("dotenv").config();
 
 app.use(express.json());
 
-// TESTING
-app.post("/create", (req, res) => {
-  const { authorization } = req.headers;
-  if (authorization === process.env.API_KEY) {
-    res.status(200).json({ message: "TESTING", auth: "Accepted" });
-  } else {
-    res.status(401).json({ message: "TESTING", auth: "Unaccepted" });
-  }
-});
-// TESTING
-app.delete("/delete", (req, res) => {
-  const { authorization } = req.headers;
-  const { id } = req.query;
-
-  if (authorization === process.env.API_KEY) {
-    console.log(id);
-    res
-      .status(200)
-      .json({ message: `TESTING. DELETE ID: ${id}`, auth: "Accepted" });
-  } else {
-    res
-      .status(401)
-      .json({ message: `TESTING. DELETE ID: ${id}`, auth: "Unaccepted" });
-  }
-});
-
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);
 });
