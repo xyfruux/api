2615622bcfa5e084c9bbf5c035116d55dcd2ac45
xyfruux <victor.stampe2010@gmail.com>
2025-05-23 22:41:01 +0200

    Create Edit Router

---
+++ src/routes/edit.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const data = require("../data.json");
+
+// TESTING
+app.post("/create", (req, res) => {
+  const { authorization } = req.headers;
+  if (authorization === process.env.API_KEY) {
+    res.status(200).json({ message: "TESTING", auth: "Accepted" });
+  } else {
+    res.status(401).json({ message: "TESTING", auth: "Unaccepted" });
+  }
+});
+// TESTING
+app.delete("/delete", (req, res) => {
+  const { authorization } = req.headers;
+  const { id } = req.query;
+
+  if (authorization === process.env.API_KEY) {
+    console.log(id);
+    res
+      .status(200)
+      .json({ message: `TESTING. DELETE ID: ${id}`, auth: "Accepted" });
+  } else {
+    res
+      .status(401)
+      .json({ message: `TESTING. DELETE ID: ${id}`, auth: "Unaccepted" });
+  }
+});
