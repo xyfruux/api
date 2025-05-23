596fec13e0452d66201b41ac4b744f3b13f42d7e
xyfruux <victor.stampe2010@gmail.com>
2025-05-23 22:32:11 +0200

    Create Api Router

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+router.get("/", (req, res) => {
+  const { minPrice, maxPrice, category } = req.query;
+
+  const filteredItems = data.filter((item) => {
+    return (
+      (!minPrice || item.price >= minPrice) &&
+      (!maxPrice || item.price <= maxPrice) &&
+      (!category || item.category == category)
+    );
+  });
+
+  res.status(200).json(filteredItems);
+});
