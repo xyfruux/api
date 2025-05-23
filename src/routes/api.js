cb1a3d07e649f933c5659a4827656d103f223b06
xyfruux <victor.stampe2010@gmail.com>
2025-05-23 22:56:52 +0200

    Require User On Api Router

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
+
+const User = require("../models/Item.js");
 
 const data = require("../data.json");
 
