7055fadfeb80f6ac940073f47061aca18c06311f
xyfruux <victor.stampe2010@gmail.com>
2025-05-24 09:02:43 +0200

    Make /create Create An Item & Add Error Handling

--- src/routes/edit.js
+++ src/routes/edit.js
@@ -14,9 +14,20 @@ router.post("/create", (req, res) => {
       description: data.description,
       category: data.category,
     });
-    console.log(data);
-    console.log(newItem);
-    res.status(200).json({ message: "TESTING", auth: "Accepted" });
+
+    newItem
+      .save()
+      .then(() => {
+        res.status(200).json({
+          message: "Item created successfully!",
+        });
+      })
+      .catch((err) => {
+        console.error(`Error creating: ${err}`);
+        res.status(500).json({
+          message: "Something went wrong creating. Please try again later.",
+        });
+      });
   } else {
     res.status(401).json({ message: "TESTING", auth: "Unaccepted" });
   }
