e8ec1a9f2ec87c4908917d72bfbddf652b7289b1
xyfruux <victor.stampe2010@gmail.com>
2025-05-27 21:37:22 +0200

    Clean If Statements With Return

--- src/routes/edit.js
+++ src/routes/edit.js
@@ -8,29 +8,29 @@ router.post("/create", (req, res) => {
   if (authorization !== process.env.API_KEY) {
     res.status(401).json({ message: "Not authorized." });
     return;
-  } else {
-    const data = req.body;
-    const newItem = new Item({
-      title: data.title,
-      price: data.price,
-      description: data.description,
-      category: data.category,
-    });
-
-    newItem
-      .save()
-      .then(() => {
-        res.status(200).json({
-          message: "Item created successfully!",
-        });
-      })
-      .catch((err) => {
-        console.error(`Error creating: ${err}`);
-        res.status(500).json({
-          message: "Something went wrong creating. Please try again later.",
-        });
-      });
   }
+
+  const data = req.body;
+  const newItem = new Item({
+    title: data.title,
+    price: data.price,
+    description: data.description,
+    category: data.category,
+  });
+
+  newItem
+    .save()
+    .then(() => {
+      res.status(200).json({
+        message: "Item created successfully!",
+      });
+    })
+    .catch((err) => {
+      console.error(`Error creating: ${err}`);
+      res.status(500).json({
+        message: "Something went wrong creating. Please try again later.",
+      });
+    });
 });
 
 router.delete("/delete", (req, res) => {
@@ -39,17 +39,18 @@ router.delete("/delete", (req, res) => {
 
   if (authorization !== process.env.API_KEY) {
     res.status(401).json({ message: "Not authorized to delete items." });
-  } else {
-    Item.deleteOne({ _id: id })
-      .then(() => {
-        res.status(200).json({ message: "Item deleted successfully!" });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "Something went wrong deleting. Please try again later.",
-        });
-      });
+    return;
   }
+
+  Item.deleteOne({ _id: id })
+    .then(() => {
+      res.status(200).json({ message: "Item deleted successfully!" });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Something went wrong deleting. Please try again later.",
+      });
+    });
 });
 
 router.put("/update", (req, res) => {
@@ -58,17 +59,18 @@ router.put("/update", (req, res) => {
 
   if (authorization !== process.env.API_KEY) {
     res.status(401).json({ message: "Not authorized to update items." });
-  } else {
-    Item.updateOne({ _id: id }, { $set: { [key]: value } })
-      .then(() => {
-        res.status(200).json({ message: "Item updated successfully!" });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "Something went wrong updating. Please try again later.",
-        });
-      });
+    return;
   }
+
+  Item.updateOne({ _id: id }, { $set: { [key]: value } })
+    .then(() => {
+      res.status(200).json({ message: "Item updated successfully!" });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Something went wrong updating. Please try again later.",
+      });
+    });
 });
 
 module.exports = router;
